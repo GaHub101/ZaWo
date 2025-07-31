8ba85d75630c2463653d7e7902d3cc2fcc01ba7c
GaHub101 <gabetig45@gmail.com>
2025-07-31 23:45:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function submitVote(choice) {
-  document.getElementById("voteValue").value = choice;
+  document.getElementById("voteValue").value = choice;  // 'Hot' oder 'Not'
   document.getElementById("voteForm").submit();
   alert("Danke für deine Bewertung!");
 }
